891a712d3fdc13fec247bcf97ac3e12467cd1e0a
Tihomir Kolev <sineadfly@gmail.com>
2025-11-09 00:11:15 +0200

    Add base layout components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,33 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Search from './components/Search'
+import UserList from './components/UserList'
+import Pagination from './components/Pagination'
 
 function App() {
     return (
         <>
-            hello
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+
+                    <UserList />
+
+                    <Pagination />
+
+                </section>
+
+                {/* User details component  */}
+
+                {/* Create/Edit Form component  */}
+
+                {/* Delete user component  */}
+            </main>
+
+            <Footer />
         </>
     )
 }
